7157239a5154271abf8c200491c4ca6f7d4567f6
Minseob So <pksomin@gmail.com>
2024-06-19 05:28:42 +0900

    다시 확인

--- src/TTTTst.java
+++ src/TTTTst.java
@@ -6,5 +6,6 @@ public class TTTTst {
         System.out.println("인텔리제이에서 대충 클릭");
         System.out.println("복제 잘됐는지 확인");
         System.out.println("동기화 테스트");
+        System.out.println("다시 확인");
     }
 }
