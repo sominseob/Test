edf353261b365f7515f378e7ddb378d7b6294adc
소민섭 <pksomin@gmail.com>
2024-06-19 05:36:23 +0900

    충돌실험

--- src/TTTTst.java
+++ src/TTTTst.java
@@ -3,10 +3,5 @@ public class TTTTst {
         System.out.println("잘돼는지 확인이나 해야징");
         System.out.println("git commit 확인");
         System.out.println("아무거나 이해했는지 테스트");
-        System.out.println("인텔리제이에서 대충 클릭");
-        System.out.println("복제 잘됐는지 확인");
-        System.out.println("신입");
-        System.out.println("새로운 브릿지 말들기");
-        System.out.println("충돌 실험");
     }
 }
