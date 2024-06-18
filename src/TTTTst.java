3efe49e242161f92368964c3e4ff4abf209a6860
Minseob So <pksomin@gmail.com>
2024-06-19 04:10:24 +0900

    인텔리 제이로 끄적

--- src/TTTTst.java
+++ src/TTTTst.java
@@ -3,5 +3,6 @@ public class TTTTst {
         System.out.println("잘돼는지 확인이나 해야징");
         System.out.println("git commit 확인");
         System.out.println("아무거나 이해했는지 테스트");
+        System.out.println("인텔리제이에서 대충 클릭");
     }
 }
