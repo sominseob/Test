66d2052ba7eb9d5d28014fa4cee3f82fa8d6022a
소민섭 <pksomin@gmail.com>
2024-06-19 05:47:26 +0900

    신입 받아라 씹련아

--- src/TTTTst.java
+++ src/TTTTst.java
@@ -3,5 +3,6 @@ public class TTTTst {
         System.out.println("잘돼는지 확인이나 해야징");
         System.out.println("git commit 확인");
         System.out.println("아무거나 이해했는지 테스트");
+        System.out.println("데했데했");
     }
 }
