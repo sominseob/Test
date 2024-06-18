db8a6d570226e27cc13215b10365cf9c06978045
Minseob So <pksomin@gmail.com>
2024-06-19 04:00:44 +0900

    일단 아무거나 해봄

--- src/TTTTst.java
+++ src/TTTTst.java
@@ -2,5 +2,6 @@ public class TTTTst {
     public static void main(String[] args) {
         System.out.println("잘돼는지 확인이나 해야징");
         System.out.println("git commit 확인");
+        System.out.println("아무거나 이해했는지 테스트");
     }
 }
